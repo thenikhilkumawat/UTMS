675682aabe4ecb605c16cffa77155cd483f964da
Nikhil kumawat <110445217+thenikhilkumawat@users.noreply.github.com>
2026-04-10 01:10:09 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
 import os
 
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-workers = 1
-worker_class = "sync"
+workers = 2
+threads = 2
+worker_class = "gthread"
 timeout = 120
 keepalive = 5
 max_requests = 1000
