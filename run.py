e556f49e1843458c2dd8967a3786b41f162141d8
Nikhil kumawat <110445217+thenikhilkumawat@users.noreply.github.com>
2026-07-01 20:49:44 +0530

    chore: trigger deploy to remove Nginx auth 2026-07-01 15:19:44

--- run.py
+++ run.py
@@ -107,4 +107,4 @@ if __name__ == "__main__":
     print("="*50 + "\n")
     app.run(debug=True, host="0.0.0.0", port=5000)
 
-# restart-trigger 2026-06-30 14:22:14
+# restart-trigger 2026-07-01 15:19:44
