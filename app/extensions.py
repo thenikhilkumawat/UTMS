4599065e72ded961cc045b21a7c4679770a01c56
Nikhil <thenikhilkumawat@gmail.com>
2026-07-13 08:24:36 +0000

    Security hardening + commission prefill

---
+++ app/extensions.py
@@ -0,0 +1,8 @@
+try:
+    from flask_limiter import Limiter
+    from flask_limiter.util import get_remote_address
+    limiter = Limiter(key_func=get_remote_address, default_limits=[], storage_uri="memory://")
+    LIMITER_AVAILABLE = True
+except ImportError:
+    limiter = None
+    LIMITER_AVAILABLE = False
